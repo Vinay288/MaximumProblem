d9d76c764aaff7056300f49a73f0069eaf515f2b
Vinay288 <vinayhiremath288@gmail.com>
2021-08-30 01:37:50 +0530

    ADDED:method to find maximum of three float numbers

--- src/com/bridgelabz/MaximumProbelm.java
+++ src/com/bridgelabz/MaximumProbelm.java
@@ -3,8 +3,26 @@ package com.bridgelabz;
 public class MaximumProbelm {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Integer firstNumber = 2;
+		Integer secondNumber = 20;
+		Integer thirdNumber = 9;
+		maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
 
+		Float firstFloat = 2.55f;
+		Float secondFloat = 1.23f;
+		Float thirdFloat = 2.12f;
+		findMaxOfThree(firstFloat, secondFloat, thirdFloat);
 	}
 
+	public static void maxOfThreeNumbers(Integer one, Integer two, Integer three) {
+		Integer maximumOfTwoIntegers = (one.compareTo(two) > 0 ? one : two);
+		Integer maximumInteger = (one.compareTo(three) > 0 ? one : three);
+		System.out.println("Maximum of three integers are:" + maximumInteger);
+	}
+
+	public static void findMaxOfThree(Float one, Float two, Float three) {
+		Float maximumOfTwoFloats = (one.compareTo(two) > 0 ? one : two);
+		Float maximumFloat = (one.compareTo(three) > 0 ? one : three);
+		System.out.println("Maximum of three integers are:" + maximumFloat);
+	}
 }
